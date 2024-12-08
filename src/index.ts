c1fce4fe9a9e51dc9ffcc128da5631e491e3713b
Kazuma Ito <zarigani2x@gmail.com>
2024-12-08 18:13:51 +0900

    v1.0.4

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { askGemini } from "./models/gemini";
 
 const cmd = new Command();
 
-cmd.name("ai-cli").version("1.0.3");
+cmd.name("ai-cli").version("1.0.4");
 
 cmd.arguments("[prompt...]").action(async (prompts: string[]) => {
   if (prompts.length === 0) {
